2d5cc6c9f8e8a1e3219893b9792a09a41c158edf
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-06-22 17:56:56 +0530

    update

--- src/api.js
+++ src/api.js
@@ -1,6 +1,3 @@
-const API_KEY = '34de86f08ffc4998abcdef358b8e0172';
-const BASE_URL = 'https://newsapi.org/v2';
-
 export const fetchArticles = async (category = 'general', page = 1) => {
     try {
         const response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&page=${page}&apiKey=34de86f08ffc4998abcdef358b8e0172`);

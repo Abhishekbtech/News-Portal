9270c0990877dc0979dc862567b03e73cafd0ba4
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-06-22 20:04:20 +0530

    update

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 export const fetchArticles = async (category = 'general', page = 1) => {
     try {
-        const response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&page=${page}&apiKey=34de86f08ffc4998abcdef358b8e0172`);
+        const response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&page=${page}&apiKey=34de86f08ffc4998abcdef358b8e0172`, {
+            protocol: 'https:',
+            // other options...
+          })
         if (!response.ok) {
             throw new Error(`Error fetching articles: ${response.statusText}`);
         }

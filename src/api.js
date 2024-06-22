7003dea4b7e18e5f023aa25e6ae68b734836b906
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-06-22 22:15:52 +0530

    update

--- src/api.js
+++ src/api.js
@@ -1,15 +1,19 @@
+import axios from 'axios';
+
+const API_KEY = '34de86f08ffc4998abcdef358b8e0172';
+const BASE_URL = 'https://newsapi.org/v2';
+
 export const fetchArticles = async (category = 'general', page = 1) => {
     try {
-        const response = await fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&page=${page}&apiKey=34de86f08ffc4998abcdef358b8e0172`, {
-            protocol: 'https:',
-            // other options...
-          })
-        if (!response.ok) {
-            throw new Error(`Error fetching articles: ${response.statusText}`);
-        }
-        const data = await response.json();
-        console.log(data)
-        return data.articles;
+        const response = await axios.get(`${BASE_URL}/top-headlines`, {
+            params: {
+                category,
+                page,
+                apiKey: API_KEY,
+                country: 'us',
+            },
+        });
+        return response.data.articles;
     } catch (error) {
         console.error("Error fetching articles:", error);
         throw error;

013d088dd77c1e6b126263a5cfe15668d937f4a2
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-06-22 12:40:09 +0530

    update

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -11,11 +11,13 @@ const ArticleCard = ({ article }) => {
 
     return (
         <div className="article-card">
-            <img src={article.urlToImage} alt={article.title} height={200} width={295}/>
+            <img src={article.urlToImage} alt={'No Image Found'} height={200} width={295}/>
             <h2>{article.title}</h2>
             <p>{article.description}</p>
-            <button onClick={handleReadMore}>Read more</button>
-            <FavoriteButton article={article} />
+            <div className='nested-div'>
+                <button onClick={handleReadMore}>Read more</button>
+                <FavoriteButton article={article} />
+            </div>
         </div>
     );
 };
